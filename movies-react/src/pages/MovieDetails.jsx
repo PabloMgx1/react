88df4f16f9ccc2bf802cf6d6c953610d746fd4c6
Pablo <pablodawf@gmail.com>
2021-12-28 17:48:35 +0100

    Actualizado

---
+++ movies-react/src/pages/MovieDetails.jsx
@@ -0,0 +1,6 @@
+import movie from "./movie.json";
+import styles from "./Moviedetails.module.css";
+
+export function MovieDetails(){
+    return <div className={styles.detailsContainer}>Details</div>;
+}
